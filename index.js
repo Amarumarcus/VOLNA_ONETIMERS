f36009d36bfa9e3acc66a54609ed3fae5f6a1618
Amarumarcus <144006456+Amarumarcus@users.noreply.github.com>
2023-09-04 16:58:25 +0300

    Updated VOLNA

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
+let passes = 0
 
 function increment() {
     count += 1
@@ -8,8 +9,9 @@ function increment() {
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
+    passes += 1
+    let countStr = "Проход " + passes + ":  " + count + " чел. "
+    saveEl.innerHTML += countStr + "<br>"
     countEl.textContent = 0
     count = 0
 }
